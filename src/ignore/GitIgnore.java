dc103ac9f8890c6d423271dbfdc8b981aec79793
aghassi kohistani <aghassikohistani@gmail.com>
2021-06-30 00:16:29 -0700

    I am trying to learn git

---
+++ src/ignore/GitIgnore.java
@@ -0,0 +1,13 @@
+package ignore;
+
+public class GitIgnore {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("my git folder");
+	}
+
+}
---
+++ src/ignore/Taqdeenadin.java
@@ -0,0 +1,12 @@
+package ignore;
+
+public class Taqdeenadin {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("this is my home project");
+
+	}
+
+}
